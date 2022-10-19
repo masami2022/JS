ff6d5a04bc3382918bd9395ffc4b5d5ebaeba9d5
masami2022 <rkmk2024@gmail.com>
2022-10-19 21:44:21 +0900

    イベント発火基礎編

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
 
+window.addEventListener('load' , function(){
+  const pullDownButton = document.getElementById("lists")
+})
+console.log('pullDownButton')
 
+pullDownButton = addEventListener('mouseout' , function(){
+  console.log("外れる")
+})
+
+pullDownButton = addEventListener('click' , function(){
+  console.log("クリック")
+})
 
 
