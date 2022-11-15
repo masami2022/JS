8bdb0e11758d2a652ebf6fc3be4c9e006750ab72
masami2022 <rkmk2024@gmail.com>
2022-11-15 20:56:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 window.addEventListener('load' , function(){
   const pullDownButton = document.getElementById("lists")
 })
-console.log('pullDownButton')
 
-pullDownButton = addEventListener('mouseout' , function(){
-  console.log("外れる")
+pullDownButton.addEventListener('mouseover' , function(){
+  pullDownButton.setAttribute("style", "background-color:blue;")
+ 
 })
 
-pullDownButton = addEventListener('click' , function(){
-  console.log("クリック")
+pullDownButton.addEventListener('click' , function(){
+  pullDownButton.setAttribute("style" , "background-color:green;")
 })
 
-
+pullDownButton.addEventListener('mouseout' ,function(){
+  pullDownButton.removeAttribute("style", "background-color:red;")
+})
