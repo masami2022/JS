1580fa84dd6557e740839fad6395ddfb215e60ab
masami2022 <rkmk2024@gmail.com>
2022-12-15 21:10:41 +0900

    Update script.js
    イベント発火基礎編

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
 window.addEventListener('load' , function(){
   const pullDownButton = document.getElementById("lists")
+  pullDownButton.addEventListener('mouseover' , function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+    console.log("乗った時青")
+    pullDownButton.addEventListener('mouseout' ,function(){
+      pullDownButton.removeAttribute("style", "background-color:red;")
+      console.log("外れた時赤")
+      pullDownButton.addEventListener('click' , function(){
+        pullDownButton.setAttribute("style" , "background-color:green;")
+        console.log("クリックすると緑")
+      })
+
+    })
+   })
 })
 
-pullDownButton.addEventListener('mouseover' , function(){
-  pullDownButton.setAttribute("style", "background-color:blue;")
- 
-})
 
-pullDownButton.addEventListener('click' , function(){
-  pullDownButton.setAttribute("style" , "background-color:green;")
-})
 
-pullDownButton.addEventListener('mouseout' ,function(){
-  pullDownButton.removeAttribute("style", "background-color:red;")
-})
+
+
+
