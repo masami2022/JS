f567e551e7633af7f3b98d4427ca60331d1aeb48
masami2022 <rkmk2024@gmail.com>
2023-01-09 15:43:27 +0900

    基礎11

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-
 window.addEventListener('load' , function(){
   const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
   pullDownButton.addEventListener('mouseover' , function(){
-    pullDownButton.setAttribute("style", "background-color:blue;")
-    console.log("乗った時青")
-    pullDownButton.addEventListener('mouseout' ,function(){
-      pullDownButton.removeAttribute("style", "background-color:red;")
-      console.log("外れた時赤")
-      pullDownButton.addEventListener('click' , function(){
-        pullDownButton.setAttribute("style" , "background-color:green;")
-        console.log("クリックすると緑")
+    pullDownButton.setAttribute("style" , "background-color:blue;")
+    console.log("青")
+    pullDownButton.addEventListener('mouseout' , function(){
+      pullDownButton.removeAttribute("style" , "background-color:red;")
+      console.log("赤に戻る")
+      pullDownButton.addEventListener('click' , function() {
+       if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style" , "display:block;")
+        console.log("非表示")
+        } else {
+          pullDownParents.setAttribute("style" , "display:block;")
+          console.log("表示")
+          }
+        })
       })
-
     })
-   })
-})
-
-
-
-
-
-
+  })  
